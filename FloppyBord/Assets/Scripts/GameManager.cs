8cbb254df2c64ffcc6c9445609457a73e6ed321d
TrachScor6 <liam.huynh@mail.utoronto.ca>
2025-04-27 16:31:31 -0400

    game works!
    i was not in inspector but i was in debug the whole time. this fixes a lot.

--- FloppyBord/Assets/Scripts/GameManager.cs
+++ FloppyBord/Assets/Scripts/GameManager.cs
@@ -1,14 +1,56 @@
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour
-{
+public class GameManager : MonoBehaviour {
+
+    public Player player;
+    public Text scoreText;
+    public GameObject playButton;
+    public GameObject gameOver;
     private int score;
+
+    private void Awake() {
+        Application.targetFrameRate = 60;
+        
+        Debug.Log("Awake");
+
+        gameOver.SetActive(false);  // hide the game over screen
+        playButton.SetActive(true); // show play button at start
+        Pause();
+    }
+
+    public void Play() {
+        
+        player.transform.position = new Vector3(0, 0, 0);
+        score = 0;
+        scoreText.text = score.ToString();
+
+        gameOver.SetActive(false);
+        playButton.SetActive(false);
+
+        Time.timeScale = 1f;
+        player.enabled = true;
+        PipeMove[] pipes = FindObjectsOfType<PipeMove>();
+
+        for (int i = 0; i < pipes.Length; i++) {
+            Destroy(pipes[i].gameObject);
+        }
+    }
+
+    public void Pause() {
+        Time.timeScale = 0f; // time doesn't update
+        player.enabled = false;
+    }
 
     public void IncreaseScore() {
         score++;
+        scoreText.text = score.ToString();
     }
 
     public void GameOver() {
-        Debug.Log("Game Over");
+        gameOver.SetActive(true);
+        playButton.SetActive(true);
+
+        Pause();
     }
 }
